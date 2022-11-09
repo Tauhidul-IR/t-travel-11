6ed626803d81ac722946315153c178a91e8e2f29
Tauhidul Islam Robin <robin13690@gmail.com>
2022-11-09 23:18:43 +0600

    add spinner on service page

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -5,26 +5,34 @@ import SingleService from './SingleService';
 
 const Services = () => {
     const [services, setServices] = useState([]);
+    const [loading, setLoading] = useState(true)
     useTitle('services')
 
     useEffect(() => {
         fetch('http://localhost:5000/allServices')
             .then(res => res.json())
-            .then(data => setServices(data))
+            .then(data => {
+                setServices(data)
+                setLoading(false)
+            })
     }, [])
     return (
         <div>
-            <div className='my-24'>
-                <h1 className='text-4xl font-bold my-10'>Services</h1>
-                <div className='grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-6 mb-10'>
-                    {
-                        services.map(service => <SingleService
-                            key={service._id}
-                            service={service}
-                        ></SingleService>)
-                    }
-                </div>
-            </div>
+            {
+                loading ? <button className="btn loading">loading</button> : <>
+                    <div className='my-24'>
+                        <h1 className='text-4xl font-bold my-10'>Services</h1>
+                        <div className='grid grid-col-1 md:grid-col-2 lg:grid-cols-3 gap-6 mb-10'>
+                            {
+                                services.map(service => <SingleService
+                                    key={service._id}
+                                    service={service}
+                                ></SingleService>)
+                            }
+                        </div>
+                    </div>
+                </>
+            }
         </div>
     );
 };
