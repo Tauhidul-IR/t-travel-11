25c2aeb55db01813873c087cca01dd4b0e4ba540
Tauhidul Islam Robin <robin13690@gmail.com>
2022-11-09 10:13:17 +0600

    login problem solved

--- src/Resister/Login.js
+++ src/Resister/Login.js
@@ -41,7 +41,7 @@ const Login = () => {
 
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-10">
                     <h1 className="text-5xl font-bold text-center">Login</h1>
-                    <form className="card-body">
+                    <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
