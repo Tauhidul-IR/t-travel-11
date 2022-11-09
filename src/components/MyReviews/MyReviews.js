2438b64a54af22831421ca82118a12da0428c4e1
Tauhidul Islam Robin <robin13690@gmail.com>
2022-11-09 08:35:53 +0600

    show my review part complete

--- src/components/MyReviews/MyReviews.js
+++ src/components/MyReviews/MyReviews.js
@@ -1,16 +1,36 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../AuthProvider/AuthProvider';
+import MySingleReview from './MySingleReview';
 
 const MyReviews = () => {
-
+    const [reviews, setReviews] = useState([])
     const { user } = useContext(AuthContext);
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/reviews?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [user?.email])
 
     return (
         <div>
-            <h2>dsfhhhhhhhhhhhhhhhhhhhhhhhhhhvhndsfvsdh
-                sdkjfdjkshn
-                jkfsdfj
-            </h2>
+            <div>
+                {
+                    reviews.length === 0 ? <> <h1>Not Review</h1></>
+                        : <>
+                            <h2>total review {reviews.length}</h2>
+                            <div className='grid grid-col-1 justify-center mb-10'>
+                                {
+                                    reviews.map(review => <MySingleReview
+                                        key={review._id}
+                                        review={review}
+                                    ></MySingleReview>)
+                                }
+                            </div>
+                        </>
+                }
+            </div>
         </div>
     );
 };
